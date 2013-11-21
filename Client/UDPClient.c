699f4db874e00c2207e1ef75ff25e93dda12d894
Cory R Siebler <cory.siebler@gmail.com>
2013-11-20 22:30:52 -0700

    Formatted Syntax

--- Client/UDPClient.c
+++ Client/UDPClient.c
@@ -9,87 +9,87 @@
 #define STR_SIZE 6	/* Length of String to represent States */
 
 struct request {
-	char client_ip[16]; /* To hold client IP address in dotted decimal */
-	int inc; /* Incarnation number of client */
-	int client; /* Client number */
-	int req; /* Request number */
-	char c; /* Random character client sends to server */
+    char client_ip[16]; /* To hold client IP address in dotted decimal */
+    int inc; /* Incarnation number of client */
+    int client; /* Client number */
+    int req; /* Request number */
+    char c; /* Random character client sends to server */
 };
 
 void DieWithError(const char *errorMessage) {
-	/* External error handling function */
-	perror(errorMessage);
-	exit(1);
+    /* External error handling function */
+    perror(errorMessage);
+    exit(1);
 }
 
 struct request* randomRequest() {
-	struct request* req = malloc(sizeof *req);
-	
-	return req;
+    struct request* req = malloc(sizeof *req);
+
+    return req;
 }
 
 int main(int argc, char *argv[]) {
-	int sock; /* Socket descriptor */
-	struct sockaddr_in serverAddr; /* Echo server address */
-	struct sockaddr_in fromAddr; /* Source address of echo */
-	unsigned short serverPort; /* Echo server port */
-	unsigned int fromSize; /* In-out of address size for recvfrom() */
-	char *servIP; /* IP address of server */
-	char echoBuffer[STR_SIZE]; /* Buffer for receiving echoed string */
-	int echoStringLen; /* Length of string to echo */
-	int respStringLen; /* Length of received response */
+    int sock; /* Socket descriptor */
+    struct sockaddr_in serverAddr; /* Echo server address */
+    struct sockaddr_in fromAddr; /* Source address of echo */
+    unsigned short serverPort; /* Echo server port */
+    unsigned int fromSize; /* In-out of address size for recvfrom() */
+    char *servIP; /* IP address of server */
+    char echoBuffer[STR_SIZE]; /* Buffer for receiving echoed string */
+    int echoStringLen; /* Length of string to echo */
+    int respStringLen; /* Length of received response */
 
-	if ((argc < 2) || (argc > 3)) {
-		/* Test for correct number of arguments */
-		fprintf(stderr, "Usage: %s <Server IP> [<Echo Port>]\n", argv[0]);
-		exit(1);
-	}
+    if ((argc < 2) || (argc > 3)) {
+        /* Test for correct number of arguments */
+        fprintf(stderr, "Usage: %s <Server IP> [<Echo Port>]\n", argv[0]);
+        exit(1);
+    }
 
-	servIP = argv[1]; /* First arg: server IP address (dotted quad) */
+    servIP = argv[1]; /* First arg: server IP address (dotted quad) */
 
-	if (argc == 3) {
-		/* Use given port, if any */
-		serverPort = atoi(argv[2]);
-	} else {
-		/* 7 is the well-known port for the echo service */
-		serverPort = 7;
-	}
+    if (argc == 3) {
+        /* Use given port, if any */
+        serverPort = atoi(argv[2]);
+    } else {
+        /* 7 is the well-known port for the echo service */
+        serverPort = 7;
+    }
 
-	/* Create a datagram/UDP socket */
-	if ((sock = socket(PF_INET, SOCK_DGRAM, IPPROTO_UDP)) < 0)
-		DieWithError("socket() failed");
+    /* Create a datagram/UDP socket */
+    if ((sock = socket(PF_INET, SOCK_DGRAM, IPPROTO_UDP)) < 0)
+        DieWithError("socket() failed");
 
-	/* Construct the server address structure */
-	memset(&serverAddr, 0, sizeof(serverAddr)); /* Zero out structure */
-	serverAddr.sin_family = AF_INET; /* Internet addr family */
-	serverAddr.sin_addr.s_addr = inet_addr(servIP); /* Server IP address */
-	serverAddr.sin_port = htons(serverPort); /* Server port */
-	
-	struct request* req = malloc(sizeof *req);
-//	strcpy(req->client_ip, "10.0.1.50\0");
-	req->c = 'A';
-	req->client = 0;
-	req->inc = 0;
-	req->req = 0;
-	
-	/* Send the Structure to the server */
-	if (sendto(sock, req, sizeof(*req), 0, (struct sockaddr *) &serverAddr, sizeof(serverAddr)) < 0)
-		DieWithError("sendto() sent a different number of bytes than expected");
+    /* Construct the server address structure */
+    memset(&serverAddr, 0, sizeof (serverAddr)); /* Zero out structure */
+    serverAddr.sin_family = AF_INET; /* Internet addr family */
+    serverAddr.sin_addr.s_addr = inet_addr(servIP); /* Server IP address */
+    serverAddr.sin_port = htons(serverPort); /* Server port */
 
-	/* Recv a response */
-//	fromSize = sizeof(fromAddr);
-//	if ((respStringLen = recvfrom(sock, echoBuffer, ECHOMAX, 0, (struct sockaddr *) &fromAddr, &fromSize)) != echoStringLen)
-//		DieWithError("recvfrom() failed");
+    struct request* req = malloc(sizeof *req);
+    //	strcpy(req->client_ip, "10.0.1.50\0");
+    req->c = 'A';
+    req->client = 0;
+    req->inc = 0;
+    req->req = 0;
 
-//	if (serverAddr.sin_addr.s_addr != fromAddr.sin_addr.s_addr) {
-//		fprintf(stderr, "Error: received a packet from unknown source.\n");
-//		exit(1);
-//	}
+    /* Send the Structure to the server */
+    if (sendto(sock, req, sizeof (*req), 0, (struct sockaddr *) &serverAddr, sizeof (serverAddr)) < 0)
+        DieWithError("sendto() sent a different number of bytes than expected");
 
-	/* null-terminate the received data */
-//	echoBuffer[respStringLen] = '\0';
-//	printf("Received: %s\n", echoBuffer); /* Print the echoed arg */
+    /* Recv a response */
+    //	fromSize = sizeof(fromAddr);
+    //	if ((respStringLen = recvfrom(sock, echoBuffer, ECHOMAX, 0, (struct sockaddr *) &fromAddr, &fromSize)) != echoStringLen)
+    //		DieWithError("recvfrom() failed");
 
-	close(sock);
-	exit(0);
+    //	if (serverAddr.sin_addr.s_addr != fromAddr.sin_addr.s_addr) {
+    //		fprintf(stderr, "Error: received a packet from unknown source.\n");
+    //		exit(1);
+    //	}
+
+    /* null-terminate the received data */
+    //	echoBuffer[respStringLen] = '\0';
+    //	printf("Received: %s\n", echoBuffer); /* Print the echoed arg */
+
+    close(sock);
+    exit(0);
 }
--- Server/UDPServer.c
+++ Server/UDPServer.c
@@ -10,96 +10,96 @@
 #define STR_SIZE 6	/* Length of String to represent States */
 
 struct request {
-	char client_ip[16]; /* To hold client IP address in dotted decimal */
-	int inc; /* Incarnation number of client */
-	int client; /* Client number */
-	int req; /* Request number */
-	char c; /* Random character client sends to server */
+    char client_ip[16]; /* To hold client IP address in dotted decimal */
+    int inc; /* Incarnation number of client */
+    int client; /* Client number */
+    int req; /* Request number */
+    char c; /* Random character client sends to server */
 };
 
 void DieWithError(const char *errorMessage) {
-	/* External error handling function */
-	perror(errorMessage);
-	exit(1);
+    /* External error handling function */
+    perror(errorMessage);
+    exit(1);
 }
 
 int main(int argc, char *argv[]) {
-	int sock; /* Socket */
-	struct sockaddr_in echoServAddr; /* Local address */
-	struct sockaddr_in clientAddr; /* Client address */
-	unsigned int cliAddrLen; /* Length of incoming message */
-	char echoBuffer[ECHOMAX]; /* Buffer for echo string */
-	unsigned short echoServPort; /* Server port */
-	int recvMsgSize; /* Size of received message */
+    int sock; /* Socket */
+    struct sockaddr_in echoServAddr; /* Local address */
+    struct sockaddr_in clientAddr; /* Client address */
+    unsigned int cliAddrLen; /* Length of incoming message */
+    char echoBuffer[ECHOMAX]; /* Buffer for echo string */
+    unsigned short echoServPort; /* Server port */
+    int recvMsgSize; /* Size of received message */
 
-	if (argc != 2) {
-		/* Test for correct number of parameters */
-		fprintf(stderr, "Usage:  %s <UDP SERVER PORT>\n", argv[0]);
-		exit(1);
-	}
+    if (argc != 2) {
+        /* Test for correct number of parameters */
+        fprintf(stderr, "Usage:  %s <UDP SERVER PORT>\n", argv[0]);
+        exit(1);
+    }
 
-	echoServPort = atoi(argv[1]); /* First arg:  local port */
+    echoServPort = atoi(argv[1]); /* First arg:  local port */
 
-	/* Create socket for sending/receiving datagrams */
-	if ((sock = socket(PF_INET, SOCK_DGRAM, IPPROTO_UDP)) < 0)
-		DieWithError("socket() failed");
+    /* Create socket for sending/receiving datagrams */
+    if ((sock = socket(PF_INET, SOCK_DGRAM, IPPROTO_UDP)) < 0)
+        DieWithError("socket() failed");
 
-	/* Construct local address structure */
-	memset(&echoServAddr, 0, sizeof (echoServAddr)); /* Zero out structure */
-	echoServAddr.sin_family = AF_INET; /* Internet address family */
-	echoServAddr.sin_addr.s_addr = htonl(INADDR_ANY); /* Any incoming interface */
-	echoServAddr.sin_port = htons(echoServPort); /* Local port */
+    /* Construct local address structure */
+    memset(&echoServAddr, 0, sizeof (echoServAddr)); /* Zero out structure */
+    echoServAddr.sin_family = AF_INET; /* Internet address family */
+    echoServAddr.sin_addr.s_addr = htonl(INADDR_ANY); /* Any incoming interface */
+    echoServAddr.sin_port = htons(echoServPort); /* Local port */
 
-	/* Bind to the local address */
-	if (bind(sock, (struct sockaddr *) &echoServAddr, sizeof(echoServAddr)) < 0)
-		DieWithError("bind() failed");
+    /* Bind to the local address */
+    if (bind(sock, (struct sockaddr *) &echoServAddr, sizeof (echoServAddr)) < 0)
+        DieWithError("bind() failed");
 
-	/* Run forever */
-	for (;;) {
-		/* Set the size of the in-out parameter */
-		cliAddrLen = sizeof (clientAddr);
-		
-		/* Allocate the Structure Memory */
-		struct request* req = malloc(sizeof *req);
-		
-		/* Allocate the Client's String */
-		char clientString[STR_SIZE] = "     \0";
-		char* clientIP;
-		int i;
+    /* Run forever */
+    for (;;) {
+        /* Set the size of the in-out parameter */
+        cliAddrLen = sizeof (clientAddr);
 
-		/* Block until receive message from a client */
-		if (recvfrom(sock, req, sizeof(*req), 0, (struct sockaddr *) &clientAddr, &cliAddrLen) < 0)
-			DieWithError("recvfrom() failed");
+        /* Allocate the Structure Memory */
+        struct request* req = malloc(sizeof *req);
 
-		/* Gather Client IP Address */
-		clientIP = inet_ntoa(clientAddr.sin_addr);
-		
-		printf("Handling client %s\n", clientIP);
-		
-		/* Assign Client IP Address to Structure Char Array */
-		for (i = 0; i < strlen(clientIP); i++) {
-			req->client_ip[i] = clientIP[i];
-		}
-		
-		if (DEBUG) {
-			printf("\n");
-			printf("NEW REQUEST\n");
-			printf("===========\n");
-			printf("CLIENT IP: %s\n", req->client_ip);
-			printf("CLIENT ID: %d\n", req->client);
-			printf("INCREMENT: %d\n", req->inc);
-			printf("CHARACTER: %c\n", req->c);
-			printf("\n");
-			printf("\n");
-		}
+        /* Allocate the Client's String */
+        char clientString[STR_SIZE] = "     \0";
+        char* clientIP;
+        int i;
 
-		/* Send String back to the client */
-//		if (sendto(sock, echoBuffer, recvMsgSize, 0, (struct sockaddr *) &clientAddr, sizeof (clientAddr)) != recvMsgSize)
-//			DieWithError("sendto() sent a different number of bytes than expected");
-		
-		/* Deallocate the Structure Memory */
-		free(req);
-		free(clientIP);
-	}
-	/* NOT REACHED */
+        /* Block until receive message from a client */
+        if (recvfrom(sock, req, sizeof (*req), 0, (struct sockaddr *) &clientAddr, &cliAddrLen) < 0)
+            DieWithError("recvfrom() failed");
+
+        /* Gather Client IP Address */
+        clientIP = inet_ntoa(clientAddr.sin_addr);
+
+        printf("Handling client %s\n", clientIP);
+
+        /* Assign Client IP Address to Structure Char Array */
+        for (i = 0; i < strlen(clientIP); i++) {
+            req->client_ip[i] = clientIP[i];
+        }
+
+        if (DEBUG) {
+            printf("\n");
+            printf("NEW REQUEST\n");
+            printf("===========\n");
+            printf("CLIENT IP: %s\n", req->client_ip);
+            printf("CLIENT ID: %d\n", req->client);
+            printf("INCREMENT: %d\n", req->inc);
+            printf("CHARACTER: %c\n", req->c);
+            printf("\n");
+            printf("\n");
+        }
+
+        /* Send String back to the client */
+        //		if (sendto(sock, echoBuffer, recvMsgSize, 0, (struct sockaddr *) &clientAddr, sizeof (clientAddr)) != recvMsgSize)
+        //			DieWithError("sendto() sent a different number of bytes than expected");
+
+        /* Deallocate the Structure Memory */
+        free(req);
+        free(clientIP);
+    }
+    /* NOT REACHED */
 }
